4eb789b4cf3abd4bf8318c278d15de885affafa4
Yoshi Watanabe <yoshi@github-noreply.com>
2017-08-01 20:49:29 -0700

    demo use of nested node_modules and module resolution.

--- typescript/module-demo/src/moduleA.ts
+++ typescript/module-demo/src/moduleA.ts
@@ -1,3 +1,4 @@
 import { Entry } from './moduleB';
+import * as colorName from 'color-name'
 
-console.log((new Entry()).name());
+console.log((new Entry()).name() + ' and RGB of Auzre is ' + colorName.azure);
